038b9f9dcc6d5dbc22546ce34f667070380088e7
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-08-03 00:25:39 +0100

    feedback incorrect corrected

--- quiz.js
+++ quiz.js
@@ -21,7 +21,7 @@ function checkAnswer() {
     if (userAnswer === correctAnswer) {
        document.getElementById('feedback').textContent = "Correct! Well done."
     } else {
-        document.getElementById('feedback').textContent = "That's incorrect. Try again"
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!"
     }
 }
 document.getElementById('submit-answer').addEventListener('click', checkAnswer)
